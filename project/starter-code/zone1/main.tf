7f57a1cea5e8919705b6f82f880ed30e70e9a639
stevenna <theyoung2002@naver.com>
2022-07-02 22:22:54 +0900

    add requried providers

--- project/starter-code/zone1/main.tf
+++ project/starter-code/zone1/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+        source  = "hashicorp/aws"
+        version = ">= 1.24.0"
+    }
+  }
+}
+
 locals {
    account_id = data.aws_caller_identity.current.account_id
 
