2ace7f84672549eec5b6e9b6ca10f4d42be77c3c
stevenna <theyoung2002@naver.com>
2022-07-09 21:08:57 +0900

    apply cluster replication

--- project/starter-code/zone1/ec2.tf
+++ project/starter-code/zone1/ec2.tf
@@ -1,13 +1,13 @@
 # aws ec2 copy-image --source-image-id ami-0b404d0db27a8f9c9 --source-region us-east-1 --region us-east-2 --name "udacity-project2"
 # {
-#     "ImageId": "ami-0d2d72e8c9aa2ff87"
+#     "ImageId": "ami-0aee2b1db728f949b"
 # }
 module "project_ec2" {
   source             = "./modules/ec2"
   instance_count     = var.instance_count
   name               = local.name
   account            = data.aws_caller_identity.current.account_id
-  aws_ami            = "ami-0d2d72e8c9aa2ff87"
+  aws_ami            = "ami-0aee2b1db728f949b"
   private_subnet_ids = module.vpc.private_subnet_ids
   public_subnet_ids  = module.vpc.public_subnet_ids
   vpc_id             = module.vpc.vpc_id
--- project/starter-code/zone1/modules/rds-s/_vars.tf
+++ project/starter-code/zone1/modules/rds-s/_vars.tf
@@ -1,3 +1,5 @@
 # variable "name" {}
 variable "vpc_id" {}
 variable "private_subnet_ids" {}
+variable "primary_db_cluster_arn" {}
+variable "primary_db_instance_arn" {}
